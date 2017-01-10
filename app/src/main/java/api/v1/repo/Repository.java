c2d0e7451741613c67f386857ccd9d22bb4d2cec
Ken Lyon <lyonk6@unlv.nevada.edu>
2017-01-09 20:44:07 -0800

    Removed tab chars

--- app/src/main/java/api/v1/repo/Repository.java
+++ app/src/main/java/api/v1/repo/Repository.java
@@ -2,13 +2,10 @@ package api.v1.repo;
 import api.v1.error.BusinessException;
 import api.v1.error.SystemException;
 
-
 public interface Repository<TaskAssistantModel> {
-
 
 	TaskAssistantModel add(final TaskAssistantModel t) throws BusinessException, SystemException;
 	TaskAssistantModel get(final TaskAssistantModel t) throws BusinessException, SystemException;
 	void update(final TaskAssistantModel t) throws BusinessException, SystemException;
 	void delete(final TaskAssistantModel t) throws BusinessException, SystemException;
-
 }
